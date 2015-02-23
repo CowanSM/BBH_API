1bba463985aacdfc7c0151b2464841c0c95d5f98
Vedad Dedajic <vedaddedajic@gmail.com>
2015-02-23 19:06:10 +0000

    updates

--- api/router.js
+++ api/router.js
@@ -20,14 +20,56 @@ exports = module.exports = function(config, options)
         require('./essential/'+file)(config); 
     });
     
+    var utilsPath = require('path').join(__dirname, 'utilities');
+    fs.readdirSync(utilsPath).forEach(function(file)
+    {
+        // console.log('requiring : ' + file);
+        // require('./essential/'+file)(config); 
+        
+        var basename = path.basename('./utilities/'+file, path.extname(file));
+        
+        config[basename] = require('./utilities/'+file); 
+    });
+    
     //Vedad - include all of our custom routes
-    var customRoutes = require('path').join(__dirname, 'routes');
+    var customRoutes = path.join(__dirname, 'routes');
     if(fs.existsSync(customRoutes))
     {
         fs.readdirSync(customRoutes).forEach(function(file)
         {
-            // console.log('requiring : ' + file);
-            require('./routes/'+file)(config); 
+            requireFilesFromDirectory(path.join(customRoutes, file));
         });
     }
+    
+    function requireFilesFromDirectory(dirPath)
+    {
+        // console.log('requireFilesFromDirectory : ', dirPath);
+        
+        if(fs.lstatSync(dirPath).isDirectory())
+        {
+            // console.log('is directory, going deeper');
+            fs.readdirSync(dirPath).forEach(function(file)
+            {   
+                var currPath = path.join(dirPath, file);
+                
+                if(fs.lstatSync(currPath).isDirectory())
+                {
+                    // console.log('is directory, going deeper');
+                    requireFilesFromDirectory(currPath);
+                }
+                else
+                {
+                    // console.log('requiring file : ', currPath);
+                    require(currPath)(config); 
+                }
+            });
+        }
+        else
+        {
+            // console.log('requiring file : ', dirPath);
+            require(dirPath)(config); 
+        }
+        
+        return;
+    };
  }
